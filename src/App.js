9db1373cb48b0e31d48574fef7dccad70b7be359
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-15 11:35:46 -0400

    added content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './App.css'
+import LeftSidebar from './components/LeftSidebar'
+import RightSideBar from './components/RightSidebar'
+import CountryItem from './components/CountryItem'
+import countries from './countries'
+
+console.log(countries);
 
 const App = () => {
 
-  return <div className="flex-row"></div>
+  return (
+  <div className="flex-row">
+    <LeftSidebar />
+    <RightSideBar countries = {countries}></RightSideBar>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/CountryItem.jsx
@@ -0,0 +1,10 @@
+function CountryItem(props) {
+    return (
+        <li className = "country-item" id = {props.name}>
+            <h3>Country: { props.name }</h3>
+            <h5>Capitol: { props.capitol }</h5>
+        </li>
+    )
+}
+
+export default CountryItem
